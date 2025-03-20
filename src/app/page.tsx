f9ff8d35b4edcbdd7d8d498233d2d3cef2597b06
AyushhNK <neupaneayush3@gmail.com>
2025-03-20 19:39:57 +0545

    feat: updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Portfolio from "@/components/Portfolio";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Portfolio />
+      <Skills />
     </>
     )
 }
