97f194b644bee8956bd83902c44bb19feca9f99f
AyushhNK <neupaneayush3@gmail.com>
2025-03-21 16:07:19 +0545

    feat: update3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <About />
       <Portfolio />
       <Skills />
+      <Contact />
+      <Footer />
     </>
     )
 }
